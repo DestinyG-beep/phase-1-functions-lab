c7bd1de289281a12103700d2c079df80f7351239
mburugudestinygakenga <mburugugakenga@gmail.com>
2024-10-21 10:27:40 +0300

    edits js file

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hqLocation = 42;
+    return Math.abs(someValue - hqLocation);
+}
+
+function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    const feetPerBlock = 264;
+    return blocks * feetPerBlock;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const blocksTravelled = Math.abs(destination - start);
+    const feetPerBlock = 264;
+    return blocksTravelled * feetPerBlock;
+}
+
+function calculatesFarePrice(start, destination) {
+    const feetTravelled = distanceTravelledInFeet(start, destination);
+    
+    if (feetTravelled === 0) {
+        return 0;
+    } else if (feetTravelled > 2500) {
+        return 'cannot travel that far';
+    } else if (feetTravelled <= 400) {
+        return 0;
+    } else if (feetTravelled > 400 && feetTravelled <= 2000) {
+        return (feetTravelled - 400) * 0.02;
+    } else {
+        return 25;
+    }
+}
